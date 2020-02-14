34c839e4e40fadc42bb8e197c5f09dfed841ed45
Adriano <adrianotagliaferro@hotmail.com>
2020-02-14 09:25:00 -0300

    Added error routers.

--- src/app/server.js
+++ src/app/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 const userRouters = require('../router/user')
+const errorRouters = require('../router/error')
 
 app.use(express.json())
 
 app.use('/v1/users', userRouters)
+app.use('/v1/errors', errorRouters)
 
 module.exports = app
---
+++ src/controller/error.js
@@ -0,0 +1,19 @@
+//TODO implementar as funcionalidades de cada endpoint aqui. Criar o Model para fazer as consultas no banco.
+
+const searchError = async (req, res) => {
+
+}
+
+const remove = async (request, response) => {
+
+}
+
+const toArchive = async (request, response) => {
+
+}
+
+module.exports = {
+    searchError,
+    remove,
+    toArchive
+}
