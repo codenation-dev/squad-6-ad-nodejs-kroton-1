990812ed5abd41e3a8ca31ef085085c6a7ff3abe
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-02-25 11:56:28 -0300

    update events_number column to integer data type

--- src/app/models/Log.js
+++ src/app/models/Log.js
@@ -7,11 +7,10 @@ class Log extends Model {
         title: Sequelize.STRING,
         message: Sequelize.STRING,
         user_token: Sequelize.STRING,
-        events_number: Sequelize.NUMBER,
+        events_number: Sequelize.INTEGER,
         level: Sequelize.ENUM('info', 'debug', 'warning', 'error', 'critical error'),
         environment: Sequelize.STRING,
         source: Sequelize.STRING,
-        timestamp: Sequelize.DATE,
       },
       {
         sequelize,
