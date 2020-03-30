dbcdf86bf482fb5abd2f1a10c90ed14f075ee2f0
Adriano <adrianotagliaferro@hotmail.com>
2020-03-29 23:09:11 -0300

    update files.

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,26 @@
+const { DATABASE_URL = 'database' } = process.env;
+
 const Sequelize = require('sequelize');
 const config = require('../config/database');
 
 const User = require('../app/models/User');
 const Log = require('../app/models/Log');
 
-const connection = new Sequelize(config);
+let connection;
+
+if (DATABASE_URL.includes('postgres://')) {
+  connection = new Sequelize({
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+    connectionString: DATABASE_URL,
+  });
+} else {
+  connection = new Sequelize(config);
+}
 
 User.init(connection);
 Log.init(connection);
